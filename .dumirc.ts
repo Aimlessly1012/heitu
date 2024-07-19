770c8dd3badc08a63a220fa64800eb00b9ea476c
Aimlessly1012 <peco@pecodeMacBook-Pro.local>
2024-07-19 15:37:45 +0800

    init7

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'dumi';
-
+const repo = 'heitu-hook';
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'heitu-hook',
   },
+  mfsu: false,
+  // runtimePublicPath: {},
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });

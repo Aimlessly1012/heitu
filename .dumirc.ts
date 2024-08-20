6e1c90e9abc0ca391a2fcf7d1a359ff1da6b0d44
Aimlessly1012 <peco@pecodeMacBook-Pro.local>
2024-08-20 17:48:44 +0800

    feature: git page1

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'dumi';
-// const repo = 'heitu-hook';
+const repo = 'heitu-hook';
 
 export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'HeiTu',
     editLink: false,
-    logo: process.env.NODE_ENV === 'production' ? `/HeiTu.png` : '/HeiTu.png',
+    logo:
+      process.env.NODE_ENV === 'production'
+        ? `/${repo}/HeiTu.png`
+        : '/HeiTu.png',
     prefersColor: { default: 'dark', switch: false },
   },
   mfsu: false,
   // runtimePublicPath: {},
-  base: process.env.NODE_ENV === 'production' ? `/` : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? `/` : '/',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
 });

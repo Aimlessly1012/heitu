3e892f5f5a4a8e0b6ae93ff2dacf9954803bce75
Aimlessly1012 <peco@pecodeMacBook-Pro.local>
2024-08-20 18:09:48 +0800

    feature: git page2

--- .dumirc.ts
+++ .dumirc.ts
@@ -6,15 +6,12 @@ export default defineConfig({
   themeConfig: {
     name: 'HeiTu',
     editLink: false,
-    logo:
-      process.env.NODE_ENV === 'production'
-        ? `/${repo}/HeiTu.png`
-        : '/HeiTu.png',
+    logo:'/HeiTu.png',
     prefersColor: { default: 'dark', switch: false },
   },
   mfsu: false,
   // runtimePublicPath: {},
-  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
-  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  base: '/',
+  publicPath: '/',
   exportStatic: {}, // 将所有路由输出为 HTML 目录结构，以免刷新页面时 404
 });
